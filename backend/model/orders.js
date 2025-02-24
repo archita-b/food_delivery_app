60a1c01e1cb395db9f9730e777c1a9187610a8f2
archita-b <archita.bhattacharyya91@gmail.com>
2025-02-24 18:42:27 +0530

    update placeOrder api for multiple items

--- backend/model/orders.js
+++ backend/model/orders.js
@@ -1,60 +1,89 @@
 import pool from "./database.js";
 
-export async function placeOrderDB(customerId, kitchenId, itemId, numOfItems) {
+export async function placeOrderDB(customerId, items) {
   try {
     await pool.query("BEGIN");
 
-    const priceResult = await pool.query(
-      `SELECT price, stock FROM kitchen_items 
-      INNER JOIN items ON kitchen_items.item_id = items.id
-      WHERE kitchen_id = $1 AND item_id = $2`,
-      [kitchenId, itemId]
+    const itemIds = items.map((item) => item.item_id);
+    const itemQuantites = items.map((item) => item.quantity);
+
+    const stockAvailabilityResult = await pool.query(
+      `WITH request_orders AS (
+          SELECT * FROM unnest($1::int[], $2::int[]) 
+          AS t(item_id, quantity)
+        )
+        SELECT kitchen_items.item_id FROM kitchen_items
+        INNER JOIN request_orders
+        ON kitchen_items.item_id = request_orders.item_id
+        WHERE kitchen_items.stock >= request_orders.quantity FOR UPDATE`,
+      [itemIds, itemQuantites]
     );
 
-    if (priceResult.rowCount === 0) {
-      throw new Error("Item not found in kitchen.");
-    }
-
-    const { price, stock } = priceResult.rows[0];
-
-    if (stock < numOfItems) {
-      throw new Error("Item is out of stock.");
+    if (stockAvailabilityResult.rows.length < items.length) {
+      throw new Error("One or more items are out of stock.");
     }
 
     const orderResult = await pool.query(
-      `INSERT INTO orders (customer_id) VALUES ($1) RETURNING *`,
-      [customerId]
+      `INSERT INTO orders (customer_id,kitchen_id) VALUES ($1,$2) RETURNING id`,
+      [customerId, 1]
     );
 
     const { id: orderId } = orderResult.rows[0];
 
     const orderItemsResult = await pool.query(
-      `INSERT INTO order_items (order_id, kitchen_id, item_id, num_of_items, price)
-            VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-      [orderId, kitchenId, itemId, numOfItems, price]
+      `WITH request_orders AS (
+          SELECT * FROM unnest($1::int[], $2::int[]) 
+          AS t(item_id, quantity)
+        )
+        INSERT INTO order_items (order_id, item_id, quantity, price)
+        SELECT $3, ro.item_id, ro.quantity, i.price
+        FROM request_orders ro
+        INNER JOIN items i
+        ON ro.item_id = i.id`,
+      [itemIds, itemQuantites, orderId]
     );
 
-    const totalPrice = numOfItems * price;
-
-    const orderDetailsResult = await pool.query(
-      `UPDATE orders SET total_price = $1 WHERE id = $2`,
-      [totalPrice, orderId]
+    const updateStockResult = await pool.query(
+      `WITH request_orders AS (
+          SELECT * FROM unnest($1::int[], $2::int[]) 
+          AS t(item_id, quantity)
+        )
+          UPDATE kitchen_items 
+          SET stock = kitchen_items.stock - request_orders.quantity
+          FROM request_orders
+          WHERE kitchen_items.item_id = request_orders.item_id`,
+      [itemIds, itemQuantites]
     );
 
-    await pool.query(
-      `UPDATE kitchen_items SET stock = stock - $1 
-            WHERE kitchen_id = $2 AND item_id = $3`,
-      [numOfItems, kitchenId, itemId]
+    const updatedOrderResult = await pool.query(
+      `UPDATE orders SET total_price = (
+        SELECT SUM(quantity * price) FROM order_items 
+        WHERE order_items.order_id = orders.id
+      ), status = 'confirmed'
+      WHERE id = $1 RETURNING *`,
+      [orderId]
     );
 
     await pool.query("COMMIT");
 
-    const orderDetails = await pool.query(
-      `SELECT * FROM orders WHERE id = $1`,
-      [orderId]
-    );
+    const {
+      id,
+      customer_id,
+      delivery_partner_id,
+      kitchen_id,
+      status,
+      created_at,
+    } = updatedOrderResult.rows[0];
+    // console.log(result);
 
-    return orderDetails.rows[0];
+    return {
+      order_id: id,
+      customer_id: customer_id,
+      delivery_partner_id: delivery_partner_id,
+      kitchen_id: kitchen_id,
+      order_status: status,
+      created_at: created_at,
+    };
   } catch (error) {
     await pool.query("ROLLBACK");
     throw error;
--- backend/model/schema/schema.sql
+++ backend/model/schema/schema.sql
@@ -6,14 +6,20 @@ CREATE TABLE customers (
     full_name TEXT,
     address TEXT NOT NULL,
     lat_long POINT NOT NULL,
-    phone VARCHAR(20) NOT NULL
+    phone VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 CREATE TABLE delivery_partners (
     id SERIAL PRIMARY KEY,
     full_name TEXT,
     phone VARCHAR(20) NOT NULL,
-    is_available BOOLEAN DEFAULT TRUE
+    is_available BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 CREATE TABLE kitchens (
@@ -35,7 +41,7 @@ CREATE TABLE items (
 CREATE TABLE kitchen_items (
     kitchen_id INTEGER REFERENCES kitchens(id),
     item_id INTEGER REFERENCES items(id),
-    stock INTEGER NOT NULL,
+    stock INTEGER NOT NULL CHECK (stock >= 0), 
     PRIMARY KEY (kitchen_id, item_id)
 );
 
@@ -43,16 +49,18 @@ CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     customer_id INTEGER REFERENCES customers(id),
     delivery_partner_id INTEGER REFERENCES delivery_partners(id),
+    kitchen_id INTEGER REFERENCES kitchens(id),
     status order_status_enum DEFAULT 'pending',
-    total_price DECIMAL DEFAULT 0
+    total_price DECIMAL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 CREATE TABLE order_items (
     order_id INTEGER REFERENCES orders(id),
-    kitchen_id INTEGER NOT NULL,
-    item_id INTEGER NOT NULL,
-    num_of_items INTEGER NOT NULL CHECK (num_of_items > 0),
+    item_id INTEGER REFERENCES items(id),
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
     price DECIMAL NOT NULL CHECK (price > 0),
-    FOREIGN KEY (kitchen_id, item_id) REFERENCES kitchen_items(kitchen_id, item_id),
-    PRIMARY KEY (order_id, kitchen_id, item_id)
+    PRIMARY KEY (order_id, item_id)
 );
