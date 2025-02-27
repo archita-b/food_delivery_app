1366499f9786640a75bf4a5a8e7f013e48017f46
archita-b <archita.bhattacharyya91@gmail.com>
2025-02-27 14:02:02 +0530

    fix bug in auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,25 +1,21 @@
+import { wrapControllerWithTryCatch } from "./utils.js";
 import { checkUserExists, getSession } from "../model/users.js";
 
-export async function isLoggedIn(req, res, next) {
-  try {
-    const { sessionId } = req.cookies;
-    const activeSession = await getSession(sessionId);
+export const isLoggedIn = wrapControllerWithTryCatch(async (req, res, next) => {
+  const { sessionId } = req.cookies;
+  const activeSession = await getSession(sessionId);
 
-    if (!sessionId || !activeSession) {
-      return res.status(401).json({ error: "Invalid session." });
-    }
-
-    const doesUserExist = await checkUserExists(activeSession.userId);
-    if (!doesUserExist) {
-      return res.status(403).json({ error: "User does not exist." });
-    }
-
-    req.userId = activeSession.userId;
-    req.sessionId = sessionId;
-
-    next();
-  } catch (error) {
-    console.log("Error in auth middleware:", error.message);
-    next(error);
+  if (!sessionId || !activeSession) {
+    return res.status(401).json({ error: "Invalid session." });
   }
-}
+
+  const doesUserExist = await checkUserExists(activeSession.user_id);
+  if (!doesUserExist) {
+    return res.status(403).json({ error: "User does not exist." });
+  }
+
+  req.userId = activeSession.user_id;
+  req.sessionId = sessionId;
+
+  next();
+});
